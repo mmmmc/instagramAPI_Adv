f900c09f0eaa63abe230174ff10c2a601b5b67f7
mmmmc <mcepeda@cristoreyny.org>
2016-02-04 13:49:04 +0000

    Getting flickr pics

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,24 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "b3aca93577762a361363318839281b22";
 
 $(document).ready(function() {
-    // your code here
+    $('#search-btn').click(function() {
+        var userSelection = $('#search-query').val();
+        searchFlickr(clientID, userSelection, 10, function(photos) {
+            console.log(photos);
+            var url=photos.photo[0].url_m;
+             var i;
+        for (i = 0; i < photos.photo.length; i++){
+        $('#search-results').append("<img src="+photos.photo[i].url_m+">");
+        } 
+        
+        $('#reset-btn').click(function(){
+            $('#search-results').empty();
+            
+        })
+      
+        });
+        
+    });
 });
+ 
